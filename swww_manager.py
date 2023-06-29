cdb87d7bd4c667b1c263aa2d03ee3815293bd053
Kodlak15 <stanlcod15@gmail.com>
2023-06-28 23:10:43 -0700

    Remove unneeded files

--- swww_manager.py
+++ swww_manager.py
@@ -3,20 +3,13 @@
 import os
 from utils.getters import get_config, get_parser, get_images, get_num_images, get_directories
 from utils.setters import set_config, set_wallpaper
-from utils.mappings import gen_mappings
 
 if __name__ == "__main__":
     # Get the configuration
     try:
         config = get_config()
     except:
-        config = {
-            "current": {"directory": "./images/", "index": -1},
-            "mappings": {"./images/", None}
-        }
-
-    # Update the mappings
-    gen_mappings(config)
+        config = {"directory": "./images/", "index": -1}
 
     # Get the program arguments
     parser = get_parser()
@@ -37,18 +30,18 @@ if __name__ == "__main__":
     # If the specified directory is the same as the current one,
     # go to the next image in that directory, otherwise switch to
     # the new directory and set the index equal to 0
-    if directory == config["current"]["directory"]:
+    if directory == config["directory"]:
         n = get_num_images(directory)
         assert n > 0, f"No images found in directory: {directory}"
-        config["current"]["index"] = (config["current"]["index"] + 1) % n
+        config["index"] = (config["current"]["index"] + 1) % n
     else:
-        config["current"]["directory"] = directory 
-        config["current"]["index"] = 0 
+        config["directory"] = directory 
+        config["index"] = 0 
 
     # Set the updated configuration
     set_config(config)
 
     # Set the new wallpaper
-    index = config["current"]["index"]
+    index = config["index"]
     images = get_images(directory)
     set_wallpaper(directory, images[index])
